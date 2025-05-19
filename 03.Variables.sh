0fc5b6700f1a3e079e8d3ab3b8cbdbc0c75a19c1
Pushpa <pushpa.boya@gmail.com>
2025-05-19 23:24:51 +0530

    hello world started

---
+++ 03.Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=Balu
+Person2=Yasho
+echo "$Person1 : : Hello! How was your day"
+echo "$Person2: : It was nice"
+echo "$Person1 :: What you did for whole day?"
+echo "$Person2 :: I played with my frnds"
+echo "$Person1 :: What did you played"
+echo "$Person2:: Hide and Seek "
